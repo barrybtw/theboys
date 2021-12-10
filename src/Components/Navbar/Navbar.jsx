b4290424afdf824a2c34f6c3c387f1a272a201a2
barry <nicolai.bernhard03@gmail.com>
2021-12-10 17:07:59 +0100

    Cool button

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -21,7 +21,7 @@ function Navbar() {
   return (
     <nav>
       <div className="nav__container">
-      <div className="nav__logo">SOCIALZ</div>
+        <div className="nav__logo">SOCIALZ</div>
         <ul className="nav__links">
           {/* Buttons */}
           {links.map((element) => {
@@ -32,14 +32,14 @@ function Navbar() {
               </Link>
             );
           })}
-          <Link to="/">
-            <BsPatchPlusFill onClick={HandleCreatePost} className="nav__btn"/>
-          </Link>
+          <a href="/" className="nav__links--newpost">
+            NEW POST
+          </a>
           {/**/}
         </ul>
       </div>
     </nav>
-  )
+  );
 }
 
 export default Navbar;
